ee18edd8da0013650d3b39d6a6d74b74d2fe15c0
Ashish Rao <ashishrao2598@gmail.com>
2025-03-28 10:50:36 +0530

    Include HTTP method and request/response headers in logging.

--- lib/api_logger.rb
+++ lib/api_logger.rb
@@ -1,55 +1,66 @@
-require "api_logger/version"
-require "api_logger/configuration"
-require "api_logger/railtie" if defined?(Rails)
-require "api_logger/generators" if defined?(Rails)
+require 'api_logger/version'
+require 'api_logger/configuration'
+require 'api_logger/railtie' if defined?(Rails)
+require 'api_logger/generators' if defined?(Rails)
 
 module ApiLogger
   class Error < StandardError; end
 
   class << self
     attr_writer :configuration
-    
+
     def configuration
       @configuration ||= Configuration.new
     end
-    
+
     def configure
       yield(configuration)
     end
-    
+
     def config
       configuration
     end
-    
-    def log(endpoint:, request_params: nil, response_body: nil, response_status: nil, error_message: nil)
+
+    def log(endpoint:, http_method: nil, request_headers: nil, request_params: nil, response_body: nil,
+            response_headers: nil, response_status: nil, error_message: nil)
       return unless configuration.enabled
 
       request_params = prepare_params(request_params)
-
+      request_headers = prepare_headers(request_headers)
       response_body = prepare_response(response_body)
+      response_headers = prepare_headers(response_headers)
 
       # Create the log entry
       klass = get_model_class
       klass.create(
         endpoint: endpoint.to_s,
+        http_method: http_method,
+        request_headers: request_headers,
         request_params: request_params,
         response_body: response_body,
+        response_headers: response_headers,
         response_status: response_status,
         error_message: error_message
       )
-    rescue => e
+    rescue StandardError => e
       Rails.logger.error("ApiLogger failed to log request: #{e.message}") if defined?(Rails)
     end
-    
+
     private
-    
+
+    def prepare_headers(headers)
+      return nil if headers.nil?
+
+      headers.to_h if headers.respond_to?(:to_h)
+    end
+
     def prepare_params(params)
       return nil if params.nil?
 
       params = params.to_h if params.respond_to?(:to_h)
       params
     end
-    
+
     def prepare_response(response)
       return nil if response.nil?
 
@@ -57,14 +68,14 @@ module ApiLogger
       when String
         begin
           JSON.parse(response)
-        rescue
+        rescue StandardError
           response
         end
       else
         response
       end
     end
-    
+
     def get_model_class
       # Dynamically define model class if it doesn't exist
       model_name = configuration.table_name.classify
--- lib/api_logger/middleware.rb
+++ lib/api_logger/middleware.rb
@@ -11,37 +11,35 @@ module ApiLogger
       # Patch the Net::HTTP class
       Net::HTTP.class_eval do
         define_method(:request) do |req, body = nil, &block|
-          begin
-            # Call original request method
-            response = original_request_method.bind(self).call(req, body, &block)
+          # Call original request method
+          response = original_request_method.bind(self).call(req, body, &block)
 
-            # Only log if middleware is enabled
-            if ApiLogger.configuration.enabled && ApiLogger.configuration.use_middleware
-              ApiLogger.log(
-                endpoint: req.path,
-                http_method: req.method,
-                request_params: req.body || body,
-                request_headers: req.each_header.to_h,
-                response_body: response.body,
-                response_headers: response.each_header.to_h,
-                response_status: response.code.to_i
-              )
-            end
-            
-            response
-          rescue => e
-            # Only log if middleware is enabled
-            if ApiLogger.configuration.enabled && ApiLogger.configuration.use_middleware
-              ApiLogger.log(
-                endpoint: req.path,
-                http_method: req.method,
-                request_params: req.body || body,
-                request_headers: req.each_header.to_h,
-                error_message: e.message
-              )
-            end
-            raise e
+          # Only log if middleware is enabled
+          if ApiLogger.configuration.enabled && ApiLogger.configuration.use_middleware
+            ApiLogger.log(
+              endpoint: req.path,
+              http_method: req.method,
+              request_params: req.body || body,
+              request_headers: req.each_header.to_h,
+              response_body: response.body,
+              response_headers: response.each_header.to_h,
+              response_status: response.code.to_i
+            )
           end
+
+          response
+        rescue StandardError => e
+          # Only log if middleware is enabled
+          if ApiLogger.configuration.enabled && ApiLogger.configuration.use_middleware
+            ApiLogger.log(
+              endpoint: req.path,
+              http_method: req.method,
+              request_params: req.body || body,
+              request_headers: req.each_header.to_h,
+              error_message: e.message
+            )
+          end
+          raise e
         end
       end
     end
@@ -50,4 +48,4 @@ module ApiLogger
       @app.call(env)
     end
   end
-end 
+end
